5a0db5762aae1a1cbf18d271296aff7f1db3811d
filipdespic <despicpb12@gmail.com>
2023-12-21 13:58:01 +0100

    refactor with DI

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,12 +1,10 @@
 import { MessagesRepository } from "./messages.repository";
+import { Injectable } from "@nestjs/common";
 
+@Injectable()
 export class MessagesService {
-    messagesRepo: MessagesRepository;
 
-    constructor() {
-        //DONT DO THIS ON REAL APPS
-        this.messagesRepo = new MessagesRepository();
-    }
+    constructor(public messagesRepo: MessagesRepository) {}
 
     findOne(id: string) {
         return this.messagesRepo.findOne(id);
